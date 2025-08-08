0742d549517dc1d11a670cb4e4b56ac13a3e3f2f
Samuel Torres <samuelvianatorres@gmail.com>
2025-08-07 21:53:39 -0300

    Adicionar arquivos de projeto.

---
+++ ExercicioFixacao5/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class ExercicioFixacao5
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite Três numero em Sequência");
+            string[] numeros = Console.ReadLine().Split(' ');
+            int n1 = int.Parse(numeros[0]);
+            int n2 = int.Parse(numeros[1]);
+            int n3 = int.Parse(numeros[2]);
+
+            if (n1 < n2 && n1 < n3)
+            {
+                Console.WriteLine($"Menor = {n1}");
+            }
+            else if (n2 < n3)
+            {
+                Console.WriteLine($"Menor = {n2}");
+            }
+            else
+            {
+                Console.WriteLine($"Menor = {n3}");
+            }
+        }
+    }
+}
